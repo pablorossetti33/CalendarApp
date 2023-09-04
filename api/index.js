97b747f4075b3489028d88c75e14c00995b27077
Pablo Rossetti <prensap.ross@gmail.com>
2023-09-04 16:02:39 -0300

    build create server with Express

--- api/index.js
+++ api/index.js
@@ -1,1 +1,11 @@
-console.log("Hola Mundo!!")
+const express = require("express");
+
+
+//Create Server
+const app = express();
+
+//Listen to requests
+
+app.listen(4000, () =>{
+    console.log(`Listening on port ${ 4000 }` );
+})
