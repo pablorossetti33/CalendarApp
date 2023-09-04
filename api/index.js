78ec786a42c5a1312d09ebddd9beaf05f8c9c03c
Pablo Rossetti <prensap.ross@gmail.com>
2023-09-04 16:09:11 -0300

    build configure route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,13 @@ const express = require("express");
 //Create Server
 const app = express();
 
+//Routes
+app.get("/", (req, res) => {
+    res.json({
+        ok: true
+    })    
+});
+
 //Listen to requests
 
 app.listen(4000, () =>{
